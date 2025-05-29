d3435d7ddd3bb5566815003017d385fb4cd9bc8d
hux <huxley.millard@gmail.com>
2025-05-29 21:09:36 +0200

    chore: added gitignore and learnt about npmignore

--- bin/crumbs.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const { clearUpCrumbs } = require('../src/index');
-
-clearUpCrumbs().catch((err) => {
-  console.error('Failed to clear crumbs:', err);
-  process.exit(1);
-});
